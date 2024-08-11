d19417d41df5619eb33a36d62cb8b8cfcfeb2575
JunioBatista <juniorjob21@gmail.com>
2024-08-10 22:42:27 -0300

    dashboard

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController extends BaseController
+{
+
+    private const VIEWS_DIRECTORY = 'Home/';
+    public function index(): string
+    {
+        return view(self::VIEWS_DIRECTORY . 'index', $this->dataToView);
+    }
+}
